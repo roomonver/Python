99f8898a1fcec1b850db1e0aea00070b60a4e4d9
roomonver <roomonver@gmail.com>
2021-05-12 18:19:50 -0400

    Estadistica basica, se agrega calculo de media y mediana

--- Estadistica/Basicos/media.py
+++ Estadistica/Basicos/media.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
-
-
-#Para acceder a las funciones estadisticas se debe importar el paquete statistics
 import statistics 
 
 
 dataList = [1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10]
 print(type(dataList))
-print("Media de la lista es : % s " % (statistics.median(dataList)))
+print("Media de la lista es : % s " % (statistics.mean(dataList)))
 
 dataTuple = (1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10)
 print(type(dataTuple))
-print("Media de la tupla es : % s " % (statistics.median(dataTuple)))
+print("Media de la tupla es : % s " % (statistics.mean(dataTuple)))
 
 dataSet = set([1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10])
 print(type(dataSet))
-print("Media de la un conjunto es : % s " % (statistics.median(dataSet)))
+print("Media de la un conjunto es : % s " % (statistics.mean(dataSet)))
 
 import pandas as pd
 dataFrame = pd.DataFrame(dataList, columns =['valores'])
 print(type(dataFrame))
-print("Media del dataFrame usando Statistics es : % s " % (statistics.median(dataFrame['valores'])))
+print("Media del dataFrame usando Statistics es : % s " % (statistics.mean(dataFrame['valores'])))
 
-print("Media del dataFrame útilizando median() es : % s " % dataFrame['valores'].median())
-
-
-  
+print("Media del dataFrame útilizando mean() es : % s " % dataFrame['valores'].mean())
---
+++ Estadistica/Basicos/mediana.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+#Para acceder a las funciones estadisticas se debe importar el paquete statistics
+import statistics 
+
+
+dataList = [1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10]
+print(type(dataList))
+print("Mediana de la lista es : % s " % (statistics.median(dataList)))
+
+dataTuple = (1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10)
+print(type(dataTuple))
+print("Mediana de la tupla es : % s " % (statistics.median(dataTuple)))
+
+dataSet = set([1, 2, 3, 4, 5, 3, 6, 4, 9, 2, 4, 1, 7, 7, 8, 9, 10])
+print(type(dataSet))
+print("Mediana de la un conjunto es : % s " % (statistics.median(dataSet)))
+
+import pandas as pd
+dataFrame = pd.DataFrame(dataList, columns =['valores'])
+print(type(dataFrame))
+print("Mediana del dataFrame usando Statistics es : % s " % (statistics.median(dataFrame['valores'])))
+
+print("Mediana del dataFrame útilizando median() es : % s " % dataFrame['valores'].median())
+
+
+  
