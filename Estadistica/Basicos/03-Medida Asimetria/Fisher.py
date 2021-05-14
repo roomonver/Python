e7c026698658463987a17e1efc998f081c3a9bd4
roomonver <roomonver@gmail.com>
2021-05-14 12:31:13 -0400

    se agrega fisher y curtosis a tuplas y dataframes

--- Estadistica/Basicos/03-Medida Asimetria/Fisher.py
+++ Estadistica/Basicos/03-Medida Asimetria/Fisher.py
@@ -11,3 +11,12 @@ print( 'Asimetrica postivia: {}'.format( skew(dataList) ))
 
 dataList = [1, 2, 3, 4, 5, 6, 7, 8, 8,9, 9,9,10]
 print( 'Simetrica negativa: {}'.format( skew(dataList) ))
+
+dataTuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print( 'Simetrica: {}'.format( skew(dataTuple) ))
+
+
+import pandas as pd
+dataFrame = pd.DataFrame(dataList, columns =['valores'])
+print(type(dataFrame))
+print("Media del dataFrame usando Statistics es : % s " % (skew(dataFrame['valores'])))
--- Estadistica/Basicos/03-Medida Asimetria/curtosis.py
+++ Estadistica/Basicos/03-Medida Asimetria/curtosis.py
@@ -8,3 +8,13 @@ print( 'curtosis: {}'.format( kurtosis(dataList) ))
 print('curtosis Negativa se llama Leptocúrtica')
 print('curtosis 0 se llama Mesocúrtica')
 print('curtosis Positiva se llama Platicúrtica')
+
+
+dataTuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print( 'Simetrica: {}'.format( kurtosis(dataTuple) ))
+
+
+import pandas as pd
+dataFrame = pd.DataFrame(dataList, columns =['valores'])
+print(type(dataFrame))
+print("Media del dataFrame usando Statistics es : % s " % (kurtosis(dataFrame['valores'])))
