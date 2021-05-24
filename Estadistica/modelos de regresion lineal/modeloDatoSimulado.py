81f8360b1e63af4bd77061f296ec7baf3d255e05
roomonver <roomonver@gmail.com>
2021-05-23 20:06:44 -0400

    obtener recta de regresion

--- Estadistica/modelos de regresion lineal/modeloDatoSimulado.py
+++ Estadistica/modelos de regresion lineal/modeloDatoSimulado.py
@@ -51,6 +51,48 @@ SST = sum(data['SST'])
 print(SSR , SSD, SST)
 
 R2 = SSR/SST
-print(R2)
+print("R2 1:", R2)
 
 plt.hist((data['SSD'] - data['y_act']))
+plt.show()
+
+
+'''
+Obtener recta de regresion
+
+y = a + b * x
+b = sum( (xi - x_m)*(yi - y_m) )/ sum((xi - x_m)**2)
+a = y_m - b * x_m
+'''
+x_mean = np.mean(data['x'])
+y_mean = np.mean(data['y_act'])
+
+
+data['beta_n'] = (data['x'] - np.mean(data['x'])) * (data['y_act'] - np.mean(data['y_act']))
+data['beta_d'] = (data['x'] - x_mean)**2
+beta = sum(data['beta_n'])/sum(data['beta_d'])
+alpha = y_mean - beta * x_mean
+
+#print(alpha, beta)
+data['y_model'] = alpha + beta * data['x']
+
+
+'''
+Modelo lineal obtenido por regresion
+y = a+b*x
+'''
+SSR = sum((data['y_model']-y_mean)**2)
+SSD = sum((data['y_model']-data['y_act'])**2)
+SST = sum((data['y_act']-y_mean)**2)
+print(SSR , SSD, SST)
+
+R2 = SSR/SST
+print("R2 2:", R2)
+
+y_mean = [np.mean(y_act) for i in range(1, len(x_list)+1)]
+plt.plot(x, y_pred)
+plt.plot(x, y_act, 'ro')
+plt.plot(x, y_mean, "g")
+plt.plot(data['x'], data['y_model'])
+plt.title("Valor Actual vs Prediccion")
+plt.show()
